1cab45cc801ab94f0675f35facb56326285170c0
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-10 23:18:35 +0700

    :zap: [component]: Created SearchBar with onInputChange methods (which then emitting event)

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input @input="onInputChange" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInputChange: function(event) {
+      //console.log(event.target.value);
+      this.$emit("termChange", event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
