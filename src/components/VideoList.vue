aa3f8b9ccd5010490b82edbe2d097488a31c8228
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-12 17:16:04 +0700

    :zap: [component]: Created VideoList

---
+++ src/components/VideoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    VideoList
+    {{ videos.length }}
+    <VideoListItem v-for="video in videos" :key="video.etag" :video="video" />
+  </ul>
+</template>
+
+<script>
+import VideoListItem from "./VideoListItem";
+export default {
+  name: "VideoList",
+  components: {
+    VideoListItem
+  },
+  props: {
+    videos: Array
+  }
+};
+</script>
+
+<style scoped>
+</style>
