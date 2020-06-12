ede104f2fe3dea83618d7744f86eb695e0e32c3f
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-12 17:17:03 +0700

    [app]: Added VideoList to template and binding data downward

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,44 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange" />
+    <VideoList :videos="videos" />
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
 // Importing API_KEY
-import {API_KEY} from './config/config.json'
+import { API_KEY } from "./config/config.json";
 
 export default {
   name: "App",
-  components: { SearchBar },
+  components: { SearchBar, VideoList },
+  data() {
+    return {
+      videos: []
+    };
+  },
   methods: {
     async onTermChange(search_str) {
       try {
-          const res = await axios.get('https://www.googleapis.com/youtube/v3/search',{
-              params:{
-                  key: API_KEY,
-                  type: 'video',
-                  part: 'snippet',
-                  q: search_str
-              }
-          })
-          console.log(res)
+        const res = await axios.get(
+          "https://www.googleapis.com/youtube/v3/search",
+          {
+            params: {
+              key: API_KEY,
+              type: "video",
+              part: "snippet",
+              q: search_str
+            }
+          }
+        );
+        console.log(res);
+        this.videos = res.data.items;
       } catch (error) {
-          console.log(error)
+        console.log(error);
       }
     }
   }
